2a17dc82691f9bd05470dfa30baa7147c5a322b8
doluwamu <dboyadeitan@gmail.com>
2021-11-21 19:58:06 +0100

    Error handling on the backend and added routing

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
+// Import dependencies
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { engine } from "express-handlebars";
-import mongoose from 'mongoose'
-import Recipe from './models/recipeModel.js'
+import mongoose from "mongoose";
+import Recipe from "./models/recipeModel.js";
 
 dotenv.config();
 
+// Initialize express
 const app = express();
 
+// Set static folders
 const __dirname = path.resolve();
-
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Mongoose connection
 mongoose.connect(
-   process.env.DB_URI,
-   {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-   },
-   ()=>{
-      console.log('Connected to DB')
-   }
-)
+  process.env.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to DB");
+  }
+);
 
 // Templating engine
-app.engine(".hbs", engine({ extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    partialsDir: `${__dirname}/views/partials/`,
+  })
+);
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
 // Routes
-app.get("/", async(req, res) => {
-   try {
-      
-      const recipes = await Recipe.find({}).lean()
+app.get("/", async (req, res) => {
+  try {
+    const recipes = await Recipe.find({}).lean();
 
-      res.render("home", { title:'Recipe App', recipes });
-   } catch (error) {
-      console.log('error')
-   }
+    return res.render("home", { title: "Recipes", recipes });
+  } catch (error) {
+    console.log("error");
+    return res.render("422Page", { title: "Error 422" });
+  }
+});
 
+app.get("/recipe/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const recipe = await Recipe.findById(id).lean();
+
+    return res.render("recipe", { title: "Recipes", recipe });
+  } catch (error) {
+    console.log("error");
+    return res.render("422Page", { title: "Error 422" });
+  }
 });
 
 app.get("/about", (req, res) => {
